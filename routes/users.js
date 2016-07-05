bb5367ef4dcbb300732a3781d2da0bf8f00dbeb4
Sergio Morales <yeyox_114@hotmail.com>
2016-07-04 19:59:46 -0500

    Creacion de rutas CRUD y vista Upload

--- routes/users.js
+++ routes/users.js
@@ -4,15 +4,36 @@ module.exports = function (passport) {
   var router = express.Router();
   /* GET users listing. */
   router.get('/', function(req, res, next) {
-    res.render('app/home',{ title: 'Home user', user: req.user});
+    res.render('app/home',{ title: 'Home', user: req.user});
   });
+
+  router.get('/new', function (req, res) {
+    res.render('app/imagenes/new', { title: 'Upload', user: req.user});
+  });
+
+  router.get('/:id/edit',function (req, res) {
+
+  });
+
+  /* REST CRUD */
+  router.route('/imagenes/:id')
+    .get(function (req, res) {
+
+    })
+    .put(function (req, res) {
+
+    })
+    .delete(function (req, res) {
+
+    });
+
 
   router.get('/logout', function(req, res, next) {
     req.session.destroy(function (err) {
       if(err){
         return nex(err);
       }
-      res.redirect('/login'); //Inside a callback… bulletproof!
+      res.redirect('/login');
     });
   });
 
